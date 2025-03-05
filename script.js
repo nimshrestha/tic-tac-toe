bcacc4982f567433fe91745d323bf91bf4298597
Nim Shrestha <nimshrestha3@gmail.com>
2025-03-05 15:16:19 +0545

    Add reset game & draw condition logic & fix win conditions

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ function Gameboard() {
     const getBoard = () => board;
 
     const placeToken = (row, col, playerToken) => {
-        if(!board[row][col].getValue() === '*') return; 
+        if(!(board[row][col].getValue() === '*')) return false; 
         board[row][col].addToken(playerToken);
+        return true;
     };
 
     const printBoard = () => {
@@ -22,7 +23,14 @@ function Gameboard() {
         console.log(boardWithCellValues);
     };
 
-    return {getBoard, placeToken, printBoard};
+    const resetBoard = () => {
+        for(let i = 0; i < rows; ++i){
+            for(let j = 0; j < cols; ++j){
+                board[i][j].addToken('*');
+            }
+        }
+    }
+    return {getBoard, placeToken, printBoard, resetBoard};
 }
 
 function Cell() {
@@ -64,44 +72,81 @@ function GameController(playerOneName = `Player 1`, playerTwoName = `Player 2`) 
         console.log(`${getActivePlayer().name}'s turn.`);
     };
 
+    const resetGame = () => {
+        gameBoard.resetBoard();
+        console.log(`Reset Game`);
+    }
+
     const playRound = (row, col) => {
-        console.log(`Dropping ${getActivePlayer().name}'s token into row ${row} column ${col}`);
-        gameBoard.placeToken(row, col, getActivePlayer().token);
+        if(gameBoard.placeToken(row, col, getActivePlayer().token)) {
+            console.log(`Dropping ${getActivePlayer().name}'s token into row ${row} column ${col}`);
+            
+            // Win Conditions Check
+    
+            //row win condition
+            const winningRow = getBoard.filter((row) => {
+                return ((row[0].getValue() === row[1].getValue()) && (row[0].getValue() === row[2].getValue()) 
+                && (row[0].getValue() !== `*`));
+            });
+            
+            //col win condition
+            const winningCol = getBoard.filter((row,index) => {
+                return ((getBoard[0][index].getValue() === getBoard[1][index].getValue()) && 
+                (getBoard[0][index].getValue() === getBoard[2][index].getValue()) && 
+                (getBoard[0][index].getValue() !== `*`));
+            }); 
 
-        // Win Conditions Check
-
-        //row win condition
-        const winningRow = getBoard.filter((row) => {
-            return row[0].getValue() === row[1].getValue() === row[2].getValue() !== `*`;
-        });
-        
-        //col win condition
-        const winningCol = getBoard.filter((row,index) => {
-            return getBoard[0][index].getValue() === getBoard[1][index].getValue() === getBoard[2][index].getValue() !== `*`;
-        }); 
-
-        if(winningRow.length || winningCol.length) {
-            console.log(`${getActivePlayer().name} wins!!`);
-            gameBoard.printBoard();
-            return;
+            // console.log(winningCol);
+            // console.log(winningCol.length);
+            // console.log(winningRow.length);
+            if(winningRow.length || winningCol.length) {
+                gameBoard.printBoard();
+                console.log(`${getActivePlayer().name} wins!!`);
+                activePlayer = players[0];
+                resetGame();
+                printNewRound();
+                return;
+            }
+            
+            //diagonal win conditions
+            else if(
+                ((getBoard[0][0].getValue() === getBoard[1][1].getValue()) && 
+                (getBoard[1][1].getValue() === getBoard[2][2].getValue())
+                && (getBoard[0][0].getValue() !== `*`)) ||
+                ((getBoard[0][2].getValue() === getBoard[1][1].getValue()) && 
+                (getBoard[1][1].getValue() === getBoard[2][0].getValue())
+                && (getBoard[0][2].getValue() !== `*`))
+            ){
+                gameBoard.printBoard();
+                console.log(`${getActivePlayer().name} wins!!`);
+                activePlayer = players[0];
+                resetGame();
+                printNewRound();
+                return;
+            }
+    
+            //draw condition
+            const boardWithCellValues = getBoard.map((row) => row.map((cell) => cell.getValue()));
+            const drawBoard = boardWithCellValues.filter((row) => row.includes('*'));
+            
+            if(drawBoard.length == 0) {
+                gameBoard.printBoard();
+                console.log(`It's a Draw.`);
+                activePlayer = players[0];
+                resetGame();
+                printNewRound();
+                return;
+            }
+    
+            switchPlayerTurn();
+            printNewRound();
         }
-        
-        //diagonal win conditions
-        else if(
-            (getBoard[0][0].getValue() === getBoard[1][1].getValue() === getBoard[2][2].getValue() !== `*`) ||
-            (getBoard[0][2].getValue() === getBoard[1][1].getValue() === getBoard[2][0].getValue() !== `*`)
-        ){
-            console.log(`${getActivePlayer().name} wins!!`);
-            gameBoard.printBoard();
-            return;
+        else{
+            console.log(`Cell is already occupuied. Try again in a different cell.`);
         }
-
-
-        switchPlayerTurn();
-        printNewRound();
-    };
+    }
 
     printNewRound();
-
+    
     return {playRound, getActivePlayer, getBoard};
 }
